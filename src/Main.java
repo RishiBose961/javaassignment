cfb18076f355e7beb6d5160d46d8929eb000def9
Rishi <rishibose1901@gmail.com>
2024-07-12 19:43:42 +0530

    assignment

---
+++ src/Main.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        //Area Of Circle Java Program
+//        Scanner inputRadius  = new Scanner(System.in);
+//        int Radius = inputRadius.nextInt();
+//        double pie = 3.14;
+//        double Area = pie*Radius*Radius;
+//        System.out.println(Area);
+
+        //Area Of Triangle 1/2*b*h
+
+
+//        int breadth = inp.nextInt();
+//        int height = inp.nextInt();
+//
+//        int AreaTraingle = (breadth*height)/2;
+//
+//        System.out.println(AreaTraingle);
+
+        //Area Of Rectangle Program
+
+//        int width = inp.nextInt();
+//        int height = inp.nextInt();
+//
+//        int AreaRec = width*height;
+//        System.out.println(AreaRec);
+
+        //Area Of Isosceles Triangle
+//        int a = inp.nextInt();
+//        int b = inp.nextInt();
+//
+//        double height = Math.sqrt(a*a - (b*b/4));
+//
+//        double areaiso = b/2*height;
+//
+//        System.out.println(areaiso);
+
+        //Area Of Parallelogram
+//
+//        int b = inp.nextInt();
+//        int h = inp.nextInt();
+//
+//        int Area = b*h;
+//        System.out.println(Area);
+
+    //    Area Of Rhombus    A=pq/2
+
+//        int p = inp.nextInt();
+//        int q = inp.nextInt();
+//
+//        int Area = (p*q)/2;
+//        System.out.println(Area);
+
+        //Area Of Equilateral Triangle
+
+//        int side = inp.nextInt();
+//        double Area = Math.sqrt(3)/4*side*side;
+//
+//        System.out.println(Area);
+
+
+        //Perimeter Of Circle C=2πr
+    }
+}
